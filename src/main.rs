b791b95231df7e5a354bc5d943c82b0f2db718bd
Soroush Bolbolabady <Soroushbolbolabady@gmail.com>
2025-11-17 15:33:05 +0100

    Add Progress bar, get the user Ip and port range.

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,56 @@
+use std::i64;
 use std::net::TcpStream;
 use std::net::SocketAddr;
 use std::time::Duration;
-
+use indicatif::ProgressBar;
+use std::io;
 
 
 
 fn main() {
     
 
-    println!("Welcome to Port Scanner!");
+    println!("Welcome to Port Scanner!\n\n\n");
+
+    println!("Enter The Ip Address: ");
 
 
-    let mut ports: [i32;1024] = [0; 1024];
+    let mut target_ip = String::new();
+    io::stdin().read_line(&mut target_ip);
+    let target_ip = target_ip.trim();
 
-    for i in 1..1024{
-        ports[i] = i as i32;
-    }
 
-    for port in ports{
+    println!("Enter Range Port:(0 - n) ");
 
-        let address: SocketAddr = format!("56.228.31.38:{}", port.to_string()).parse().unwrap();
+    let mut final_port = String::new();
+    io::stdin().read_line(&mut final_port);
+
+
+    let final_port = final_port.trim().parse::<i64>().unwrap();
+    let mut open_ports = Vec::new();
+
+
+    let mut port = 1;
+    
+    let bar = ProgressBar::new(final_port as u64);
+
+    while port <= final_port {
+       bar.inc(1); 
+        let address: SocketAddr = format!("{}:{}", target_ip, port).parse().unwrap();
         
-        let timeout = Duration::from_millis(70);
+        let timeout = Duration::from_millis(60);
         let result = TcpStream::connect_timeout(&address, timeout);
-       
-
         match result {
             Ok(_stream) => {
-                println!("This address is Open: {}", &address)
+                open_ports.push(port);
             }
-            Err(_e)=>{
-                continue;
-            }
-            
+            Err(_e)=>{}
         }
+        port += 1;
+
     }
-
-
-
-} 
+    bar.finish();
+        
+    println!("\n=== Summary ===");
+    println!("Found {} open ports: {:?}", open_ports.len(), open_ports);
+}
